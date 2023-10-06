0e8ad2da06bb13b66191dd3fa66d470906a838a1
Sergio Arturo <119349223+sergioarturola@users.noreply.github.com>
2023-10-05 22:22:58 -0600

    Delete contar_nums.py

--- contar_palabrasFile.py
+++
@@ -1,8 +0,0 @@
-file = open("Archivo1.txt", "r")
-num_caracteres = file.read()
-#para quitar los espacios en blanco
-letras = num_caracteres.split()
-#para obtener el numero total
-numero_palabras = len(letras)
-
-print("El numero de palabras del archivo es: ", numero_palabras)
